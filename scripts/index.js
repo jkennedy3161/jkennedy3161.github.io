0d931e38d301b725179cfcdc15f4679fe8defa17
jkennedy3161 <kennedyjt88@gmail.com>
2017-10-30 10:39:46 -0700

    add script when web document is ready, every loads on the page

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  // do things
+  $('#navbar').children().children().children().css('padding', '14px 30px').css('margin-right', '12px');
+  $('#social').children().css('color', '#333').css('text-decoration', 'none');
+});
